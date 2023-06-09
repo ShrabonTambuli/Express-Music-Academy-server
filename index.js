17a7f79abd280cb9b9abb7940e63793b76fb8c7b
ShrabonTambuli <shrabonsa@gmail.com>
2023-06-09 20:13:08 +0600

    users data post

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER
-}:${process.env.DB_PASS}@cluster0.ezafyme.mongodb.net/?retryWrites=true&w=majority`;
+  }:${process.env.DB_PASS}@cluster0.ezafyme.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,49 +27,61 @@ const classCollection = client.db('expressMusicAcademy').collection('popularClas
 const instructorCollection = client.db('expressMusicAcademy').collection('popularInstructors');
 const classesCollection = client.db('expressMusicAcademy').collection('classes');
 const selectedCollection = client.db('expressMusicAcademy').collection('selected');
+const usersCollection = client.db('expressMusicAcademy').collection('users');
 
+
+app.post('/users', async (req, res) => {
+  const user = req.body;
+  const query = { email: user.email }
+  const existingUser = await usersCollection.findOne(query);
+  if (existingUser) {
+    return res.send({ message: 'user already existing' })
+  }
+  const result = await usersCollection.insertOne(user);
+  res.send(result);
+});
 
 app.get('/class', async (req, res) => {
-    const cursor = classCollection.find();
-    const result = await cursor.toArray();
-    res.send(result);
-  })
+  const cursor = classCollection.find();
+  const result = await cursor.toArray();
+  res.send(result);
+})
 
 app.get('/instructors', async (req, res) => {
-    const cursor = instructorCollection.find();
-    const result = await cursor.toArray();
-    res.send(result);
-  })
+  const cursor = instructorCollection.find();
+  const result = await cursor.toArray();
+  res.send(result);
+})
 
 app.get('/classes', async (req, res) => {
-    const cursor = classesCollection.find();
-    const result = await cursor.toArray();
-    res.send(result);
-  })
+  const cursor = classesCollection.find();
+  const result = await cursor.toArray();
+  res.send(result);
+})
 
 
 app.get('/selected', async (req, res) => {
-    const email = req.query.email;
-    if(!email){
-      res.send([]);
-    }
-    const query = {email: email};
-    const result = await selectedCollection.find(query).toArray();
-    res.send(result);
-  })
+  const email = req.query.email;
+  if (!email) {
+    res.send([]);
+  }
+  const query = { email: email };
+  const result = await selectedCollection.find(query).toArray();
+  res.send(result);
+})
 
 app.post('/selected', async (req, res) => {
-    const p = req.body;
-    const result = await selectedCollection.insertOne(p);
-    res.send(result);
-  })
+  const p = req.body;
+  const result = await selectedCollection.insertOne(p);
+  res.send(result);
+})
 
-  app.delete('/selected/:id', async (req, res) =>{
-    const id = req.params.id;
-    const query = {_id: new ObjectId(id)};
-    const result = await selectedCollection.deleteOne(query);
-    res.send(result);
-  })
+app.delete('/selected/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await selectedCollection.deleteOne(query);
+  res.send(result);
+})
 
 
 
@@ -95,9 +107,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Express Music Academy Server is updating......!!')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Express Music Academy Server is running port:${port}`)
-  })
+  res.send('Express Music Academy Server is updating......!!')
+})
+
+app.listen(port, () => {
+  console.log(`Express Music Academy Server is running port:${port}`)
+})
